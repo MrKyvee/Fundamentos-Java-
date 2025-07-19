d9f47668af4d0084f93c98d9940f2a0bf19f8790
MrKyvee <mrkyveedeveloper@gmail.com>
2025-07-19 13:27:34 -0300

    Entrada de Dados

--- EntradaDeDados/Entrada.java
+++ EntradaDeDados/Entrada.java
@@ -1,4 +1,12 @@
 package EntradaDeDados;
 
+import java.util.Scanner;
+
 public class Entrada {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("Valor da variavel a: " + a);
+        sc.close();
+    }
 }
