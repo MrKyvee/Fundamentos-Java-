d57dc02c9d46355133b7d7e8bcd6fdfa472e8ee1
MrKyvee <mrkyveedeveloper@gmail.com>
2025-07-20 11:02:16 -0300

    Ternario

--- Condicionais/Ternarios.java
+++ Condicionais/Ternarios.java
@@ -4,11 +4,17 @@ import java.util.Scanner;
 
 public class Ternarios {
     public static void main(String[] args) {
-        System.out.print("Digite um numero: ");
+        System.out.print("Digite um número: ");
         Scanner sc = new Scanner(System.in);
         int numero = sc.nextInt();
-        //Ternario
-        String parOuImpar = (numero % 2 == 0) ? "é par": "é Impar";
+
+        // Ternário:
+        // É uma forma abreviada de escrever um if-else que retorna um valor
+        // ao invés de executar um bloco de código.
+        // Sintaxe: SeCondiçãoForVerdadeira ? valor : valor
+        //? Então retorna : Senão retorna
+        String parOuImpar = (numero % 2 == 0) ? "é par" : "é ímpar";
+
         System.out.println(parOuImpar);
         sc.close();
     }
