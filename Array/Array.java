af9476770f682a9d3291eb021207317f97d4334d
MrKyvee <mrkyveedeveloper@gmail.com>
2025-07-21 17:01:40 -0300

    Arrays

--- Array/Array.java
+++ Array/Array.java
@@ -1,4 +1,11 @@
 package Array;
 
+import java.util.Scanner;
+
 public class Array {
+    public static void main(String[] args) {
+        int[] valores = new int[5];
+        // int define o tipo dos valores que serao armazenados no array
+        // [] define que é um array
+    }
 }
